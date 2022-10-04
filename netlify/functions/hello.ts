0d49ad91edb834b19045f8ab97372beeed2dc181
Noah Paessel <knowuh@gmail.com>
2022-10-04 16:29:45 -0400

    trying out env vars part 2

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,9 +1,10 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
+  const greeting = process.env.GREETING;
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: `Hello World: "${greeting}"` }),
   };
 };
 
